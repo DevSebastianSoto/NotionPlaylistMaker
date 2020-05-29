9d2f37701642f2dd1fe226174c1a01a78928b289
Sebastian Noe Soto Madrigal <dev.snsm@gmail.com>
2020-05-29 00:35:42 -0600

    Ability to create playlist and add songs to it

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from decouple import config
-from controllers import Spotify, Printable, Track
+from controllers import Spotify, Printable, Track, DataSongs, DataGenres
 
 
 def write(fname: str, data: Printable):
@@ -7,16 +7,47 @@ def write(fname: str, data: Printable):
         infile.write(data.toJSON())
 
 
-if __name__ == "__main__":
-    SCOPE = "user-read-email user-read-private \
+def init_spotify():
+    scope = "user-read-email user-read-private \
     playlist-modify-public playlist-read-private playlist-modify-private"
 
-    sp = Spotify(
+    return Spotify(
         username=config("SPUSR"),
-        scope=SCOPE,
+        scope=scope,
         client_id=config("CLIENT_ID"),
         client_secret=config("CLIENT_SECRET"),
         redirect_uri=config("REDIRECT_URI")
     )
 
-    print(sp.playlist_exists('TEST'))
+
+def create_playlist(sp: Spotify, name: str, tracks: list) -> str:
+    new_id = ""
+    playlist_id = sp.playlist_exists(name)
+    if playlist_id is None:
+        sp.create_playlist(name)
+        new_id = sp.playlist_exists(name)
+    else:
+        new_id = playlist_id
+    sp.add_track_to_playlist(playlist_id=new_id, tracks=tracks)
+    return new_id
+
+
+if __name__ == "__main__":
+    songs = DataSongs('songs.csv')
+    sp = init_spotify()
+    track_list = []
+    df = songs.with_saxophone()
+    for index, row in df.iterrows():
+        name = row['Name']
+        artist = row['Artists'][0]
+        try:
+            sptf_song = sp.search_track(
+                name=name, artist=artist)
+            song = Track(sptf_song)
+            print(song.id)
+            track_list.append(song.id)
+        except Exception as exp:
+            lmt = '---------------------------------'
+            print(f'{lmt}\n{name} by {artist} NOT FOUND\n{lmt}')
+
+    create_playlist(sp, 'Saxophone', track_list)
--- controllers.py
+++ controllers.py
@@ -1,7 +1,66 @@
-from urllib3.util import parse_url
 import json
 import spotipy
 from spotipy.util import prompt_for_user_token
+from urllib3.util import parse_url
+import pandas as pd
+from abc import ABC, abstractmethod
+
+
+class DataAccess(ABC):
+    def __init__(self, file_path: str):
+        self._df = pd.read_csv(file_path)
+        self._df = self.clean_data()
+
+    @abstractmethod
+    def data(self):
+        pass
+
+    @abstractmethod
+    def clean_data(self):
+        pass
+
+    @staticmethod
+    def filter_url_field(field: str) -> list:
+        urls = field.split(',')
+        res = []
+        for url in urls:
+            res.append(' '.join(url.split('/')[-1].split('-')[:-1]))
+        return res
+
+
+class DataGenres(DataAccess):
+    def __init__(self, file_path):
+        super(DataGenres, self).__init__(file_path)
+
+    @property
+    def data(self):
+        return self._df.astype({'Name': str, 'Genre': list})
+
+    def clean_data(self):
+        return self._df
+
+
+class DataSongs(DataAccess):
+    def __init__(self, file_path):
+        super(DataSongs, self).__init__(file_path)
+
+    @property
+    def data(self):
+        return self._df.drop(columns=['Music Sheet'])
+
+    def clean_data(self) -> pd.DataFrame:
+        df = self._df
+        df['Artists'] = df['Artists'].apply(
+            lambda row: self.filter_url_field(row))
+        df['Genre'] = df['Genre'].apply(
+            lambda row: self.filter_url_field(row))
+        return df
+
+    def __instrument_filter(self, instrument: str) -> pd.DataFrame:
+        return self._df[self._df[instrument] == 'Yes']
+
+    def with_saxophone(self) -> pd.DataFrame:
+        return self.__instrument_filter('Saxophone').drop(columns=['Saxophone'])
 
 
 class Spotify:
@@ -20,21 +79,27 @@ class Spotify:
     def playlists(self):
         return self.__instance.user_playlists(self.__username)['items']
 
-    def new_playlist(self, name, description)
+    def create_playlist(self, name, description=""):
+        self.__instance.user_playlist_create(
+            self.__username, name=name, description=description)
 
     def search_track(self, name, artist):
         query = f'{name} artist:{artist}'
         return self.__instance.search(q=query, limit=1, type='track')
 
-    def playlist_exists(self, name) -> bool:
+    def playlist_exists(self, name) -> str:
         for playlist in self.playlists:
             if playlist['name'] == name:
-                return True
-        return False
+                return playlist['id']
+        return None
+
+    def add_track_to_playlist(self, playlist_id: str, tracks: list):
+        self.__instance.user_playlist_add_tracks(
+            self.__username, playlist_id=playlist_id, tracks=tracks)
 
 
 class Printable():
-    def toJSON(self):
+    def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__,
                           sort_keys=True, indent=4)
 
@@ -42,7 +107,10 @@ class Printable():
 class Track(Printable):
     def __init__(self, jdata: dict):
         track = jdata['tracks']['items'][0]
-        self.__id = track['id']
+        self.__id = track['uri']
         self.__name = track['name']
         self.__artist_id = track['artists'][0]['id']
 
+    @property
+    def id(self):
+        return self.__id
